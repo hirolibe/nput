e36a74385bf40b0f4ecb581005fd3a84e0fe5400
hiro <hiro.libe.0930@gmail.com>
2024-09-27 16:49:21 +0900

    Firebaseのユーザー情報取得テスト

--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -1,8 +1,12 @@
 import { AppBar, Box, Button, Container } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '@/utils/firebaseConfig'
 
 const Header = () => {
+  const [user] = useAuthState(auth)
+
   return (
     <AppBar
       position="static"
@@ -26,41 +30,44 @@ const Header = () => {
               <Image src="/logo.png" width={90} height={40} alt="logo" />
             </Link>
           </Box>
-          <Box>
-            <Link href="/login">
-              <Button
-                color="primary"
-                variant="contained"
-                sx={{
-                  color: 'white',
-                  textTransform: 'none',
-                  fontSize: 16,
-                  borderRadius: 2,
-                  boxShadow: 'none',
-                  fontWeight: 'bold',
-                }}
-              >
-                ログイン
-              </Button>
-            </Link>
-            <Link href="/signup">
-              <Button
-                color="primary"
-                variant="outlined"
-                sx={{
-                  textTransform: 'none',
-                  fontSize: 16,
-                  borderRadius: 2,
-                  boxShadow: 'none',
-                  border: '1.5px solid #3EA8FF',
-                  ml: 2,
-                  fontWeight: 'bold',
-                }}
-              >
-                新規登録
-              </Button>
-            </Link>
-          </Box>
+          {!user && (
+            <Box>
+              <Link href="/login">
+                <Button
+                  color="primary"
+                  variant="contained"
+                  sx={{
+                    color: 'white',
+                    textTransform: 'none',
+                    fontSize: 16,
+                    borderRadius: 2,
+                    boxShadow: 'none',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  ログイン
+                </Button>
+              </Link>
+              <Link href="/signup">
+                <Button
+                  color="primary"
+                  variant="outlined"
+                  sx={{
+                    textTransform: 'none',
+                    fontSize: 16,
+                    borderRadius: 2,
+                    boxShadow: 'none',
+                    border: '1.5px solid #3EA8FF',
+                    ml: 2,
+                    fontWeight: 'bold',
+                  }}
+                >
+                  新規登録
+                </Button>
+              </Link>
+            </Box>
+          )}
+          {user && <Box>{user.email}</Box>}
         </Box>
       </Container>
     </AppBar>

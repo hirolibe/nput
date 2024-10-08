4f5a2aa37027fda67fdbc71ab476e4303db15cba
hiro <hiro.libe.0930@gmail.com>
2024-10-08 22:30:04 +0900

    redis接続方法の修正

--- rails/config/initializers/firebase_id_token.rb
+++ rails/config/initializers/firebase_id_token.rb
@@ -1,5 +1,5 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0"))
+  config.redis = Redis.new(url: ENV.fetch("REDIS_URL", "redis://redis:6379/0"))
   config.project_ids = [ENV["FIREBASE_PROJECT_ID"]]
 end
 

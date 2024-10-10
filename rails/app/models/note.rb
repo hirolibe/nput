65209cf05e9becb00097df16886ad835252ff0e7
hiro <hiro.libe.0930@gmail.com>
2024-10-10 19:50:41 +0900

    Noteコントローラーのshowアクションとリクエストスペックの実装

--- rails/app/models/note.rb
+++ rails/app/models/note.rb
@@ -6,9 +6,39 @@ class Note < ApplicationRecord
   validates :title, :content, :published_at, presence: true, if: :published?
   validate :validate_single_unsaved
 
-  def validate_single_unsaved
-    if unsaved? && user.notes.unsaved.exists?
-      errors.add(:base, "未保存の記事は複数保有できません")
-    end
+  def author_name
+    user.name
   end
+
+  def from_today
+    now = Time.zone.now
+    published_at = self.published_at
+
+    months = (now.year - published_at.year) * 12 + now.month - published_at.month - ((now.day >= published_at.day) ? 0 : 1)
+    years = months.div(12)
+
+    return "#{years}年前" if years > 0
+    return "#{months}ヶ月前" if months > 0
+
+    seconds = (now - published_at).round
+
+    days = seconds / (60 * 60 * 24)
+    return "#{days}日前" if days.positive?
+
+    hours = seconds / (60 * 60)
+    return "#{hours}時間前" if hours.positive?
+
+    minutes = seconds / 60
+    return "#{minutes}分前" if minutes.positive?
+
+    "#{seconds}秒前"
+  end
+
+  private
+
+    def validate_single_unsaved
+      if unsaved? && user.notes.unsaved.exists?
+        errors.add(:base, "未保存の記事は複数保有できません")
+      end
+    end
 end

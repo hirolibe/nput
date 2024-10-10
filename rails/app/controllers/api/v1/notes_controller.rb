9a77663e747212a46d4db6903fe5065a7500b8ac
hiro <hiro.libe.0930@gmail.com>
2024-10-10 12:55:33 +0900

    ノート一覧データの取得

---
+++ rails/app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::NotesController < Api::V1::ApplicationController
+  include Pagination
+
+  def index
+    notes = Note.published.order(created_at: :desc).page(params[:page] || 1).per(10)
+    render json: notes, each_serializer: NoteIndexSerializer, meta: pagination(notes), adapter: :json
+  end
+end

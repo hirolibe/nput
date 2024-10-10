a8ef611504e91712f6da8818c85b056780218a1c
hiro <hiro.libe.0930@gmail.com>
2024-10-11 08:51:48 +0900

    シリアライザー共通化のため、名称修正

--- rails/app/serializers/note_serializer.rb
+++ rails/app/serializers/note_serializer.rb
@@ -1,4 +1,4 @@
-class NoteShowSerializer < ActiveModel::Serializer
+class NoteSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :published_at, :updated_at, :author_name
   belongs_to :user, serializer: UserSerializer
 end

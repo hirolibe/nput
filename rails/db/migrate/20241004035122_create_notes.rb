d8606bd45317e8a1000c3b99e31325c09f94a2f9
hiro <hiro.libe.0930@gmail.com>
2024-10-11 11:53:55 +0900

    statusカラムの値の日本語化

--- rails/db/migrate/20241004035122_create_notes.rb
+++ rails/db/migrate/20241004035122_create_notes.rb
@@ -3,12 +3,13 @@ class CreateNotes < ActiveRecord::Migration[7.0]
     create_table :notes do |t|
       t.string :title, comment: "タイトル"
       t.text :content, comment: "本文"
-      t.integer :status, comment: "ステータス（10:未保存, 20:下書き, 30:公開中）"
-      t.datetime :published_at, comment: "公開日時"
+      t.integer :status, null: false, default: 10, comment: "ステータス（10:未保存, 20:下書き, 30:公開中）"
+      t.datetime :published_at, comment: "公開日"
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :notes, :status
     add_index :notes, :published_at
   end
 end

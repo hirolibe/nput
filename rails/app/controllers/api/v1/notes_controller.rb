1c3470cbf5a4cc6043a850fc6202fd40cb7ee319
hiro <hiro.libe.0930@gmail.com>
2024-10-12 10:52:52 +0900

    createアクションの実装

--- rails/app/controllers/api/v1/notes_controller.rb
+++ rails/app/controllers/api/v1/notes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::NotesController < Api::V1::ApplicationController
   include Pagination
+  before_action :authenticate_user!, only: :create
 
   def index
     notes = Note.published.order(created_at: :desc).page(params[:page] || 1).per(10)
@@ -9,5 +10,10 @@ class Api::V1::NotesController < Api::V1::ApplicationController
   def show
     note = Note.published.find(params[:id])
     render json: note
+  end
+
+  def create
+    unsaved_note = current_user.notes.unsaved.first || current_user.notes.create!(status: :unsaved)
+    render json: unsaved_note
   end
 end

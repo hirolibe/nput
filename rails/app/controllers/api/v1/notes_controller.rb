31c07b842db7074f508af0395c69cb7902b4f438
hiro <hiro.libe.0930@gmail.com>
2024-10-16 13:04:48 +0900

    コメントの新規作成機能（createアクション）の実装

--- rails/app/controllers/api/v1/notes_controller.rb
+++ rails/app/controllers/api/v1/notes_controller.rb
@@ -19,9 +19,18 @@ class Api::V1::NotesController < Api::V1::ApplicationController
 
   def update
     note = current_user.notes.find(params[:id])
-    filtered_params = note.published_at.present? ? delete_published_at(note_params) : note_params
-    note.update!(filtered_params)
-    render json: note
+
+    filtered_params = if note.published_at.present?
+                        delete_published_at(note_params)
+                      else
+                        note_params
+                      end
+
+    if note.update(filtered_params)
+      render json: note, status: :ok
+    else
+      render json: { errors: note.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy

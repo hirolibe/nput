6b8f6e87fb2cbf6cc0ac1b0bbf9ac7339e5831b6
hiro <hiro.libe.0930@gmail.com>
2024-10-16 15:30:38 +0900

    コメントの削除機能の実装

--- rails/spec/requests/api/v1/notes/update_spec.rb
+++ rails/spec/requests/api/v1/notes/update_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe "Api::V1::Notes PATCH /api/v1/notes/id", type: :request do
   subject { patch(api_v1_note_path(note_id), headers:, params:) }
 
   let(:current_user) { create(:user) }
-  let(:current_user_note) { create(:note, title: "タイトル", content: "本文", status: :draft, published_at: 2024 / 10 / 1, user: current_user) }
-  let(:note_id) { current_user_note.id }
+  let(:note) { create(:note, title: "タイトル", content: "本文", status: :draft, published_at: 2024 / 10 / 1, user: current_user) }
+  let(:note_id) { note.id }
   let(:headers) { { Authorization: "Bearer token" } }
   let(:params) { { "note": { "title": "更新タイトル", "content": "更新本文", "status": "published", "published_at": "2024/11/1" } } }
 
@@ -16,13 +16,13 @@ RSpec.describe "Api::V1::Notes PATCH /api/v1/notes/id", type: :request do
       stub_token_verification.and_return({ "sub" => current_user.uid })
     end
 
-    context "更新するノートのidが、ログインユーザーが作成したノートのidである場合" do
+    context "ログインユーザーが作成したノートの場合" do
       context "全てのパラメータを正しく入力した場合" do
         it "正常にレコードを更新できる" do
-          expect { subject }.to change { current_user_note.reload.title }.from("タイトル").to("更新タイトル") and
-            change { current_user_note.reload.content }.from("本文").to("更新本文") and
-            change { current_user_note.reload.status }.from("draft").to("published") and
-            change { current_user_note.reload.published_at }.from("2024/10/1").to("2024/11/1")
+          expect { subject }.to change { note.reload.title }.from("タイトル").to("更新タイトル") and
+            change { note.reload.content }.from("本文").to("更新本文") and
+            change { note.reload.status }.from("draft").to("published") and
+            change { note.reload.published_at }.from("2024/10/1").to("2024/11/1")
           expect(json_response.keys).to eq ["id", "title", "content", "status_jp", "published_date", "updated_date", "author_name"]
           expect(response).to have_http_status(:ok)
         end
@@ -49,10 +49,9 @@ RSpec.describe "Api::V1::Notes PATCH /api/v1/notes/id", type: :request do
       end
     end
 
-    context "更新するノートのidが、ログインユーザーが作成したノートのidではない場合" do
+    context "ログインユーザーが作成したノートではない場合" do
       let(:other_user) { create(:user) }
-      let(:other_user_note) { create(:note, user: other_user) }
-      let(:note_id) { other_user_note.id }
+      let(:note) { create(:note, user: other_user) }
 
       it "例外が発生する" do
         expect { subject }.to raise_error(ActiveRecord::RecordNotFound)

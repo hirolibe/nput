994212b8090d476af5f9270ac6d68e69a6d52222
hiro <hiro.libe.0930@gmail.com>
2024-10-23 12:24:09 +0900

    アカウントの削除機能の実装

--- rails/app/controllers/api/v1/users_controller.rb
+++ rails/app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,22 @@
 class Api::V1::UsersController < Api::V1::ApplicationController
+  before_action :authenticate_user!, only: [:destroy]
+
   def show
     user = User.find(params[:id])
-
     render json: user, status: :ok
   rescue ActiveRecord::RecordNotFound
     render json: { error: "アカウントが見つかりません" }, status: :not_found
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user == current_user
+      current_user.destroy!
+      render json: { message: "アカウントを削除しました" }, status: :ok
+    else
+      render json: { error: "他のアカウントは削除できません" }, status: :forbidden
+    end
+  rescue ActiveRecord::RecordNotDestroyed
+    render json: { error: "アカウントの削除に失敗しました" }, status: :unprocessable_entity
   end
 end

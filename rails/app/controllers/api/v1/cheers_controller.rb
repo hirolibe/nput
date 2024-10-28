ac1e15a56c32bd73432b097b9f202f85eb448ecc
hiro <hiro.libe.0930@gmail.com>
2024-10-28 10:16:15 +0900

    Cheersコントローラーのdestroyエンドポイントの実装

--- rails/app/controllers/api/v1/cheers_controller.rb
+++ rails/app/controllers/api/v1/cheers_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CheersController < Api::V1::ApplicationController
-  before_action :authenticate_user!, only: [:show, :create]
+  before_action :authenticate_user!, only: [:show, :create, :destroy]
 
   def show
     note = Note.published.find(params[:note_id])
@@ -25,18 +25,35 @@ class Api::V1::CheersController < Api::V1::ApplicationController
     render json: { error: "ノートにアクセスできません" }, status: :not_found
   end
 
+  def destroy
+    note = Note.published.find(params[:note_id])
+    cheer = current_user.cheers.find_by(note_id: note.id)
+
+    if cheer
+      cheer.destroy!
+      render json: { cheer_status: false }, status: :ok
+    else
+      render json: { error: "まだこのノートにエールしていません" }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotDestroyed
+    render json: { error: "エールの削除に失敗しました" }, status: :unprocessable_entity
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "ノートにアクセスできません" }, status: :not_found
+  end
+
   private
 
     def handle_cheer(note)
-      current_user.cheers.create!(note_id: note.id)
-      current_user.profile.cheer_points -= 1
+      ActiveRecord::Base.transaction do
+        current_user.cheers.create!(note_id: note.id)
+        current_user.profile.cheer_points -= 1
 
-      if current_user.profile.save
-        cheer_status = current_user.has_cheered?(note)
-        render json: { cheer_status: }, status: :created
-      else
-        render json: { error: "保有エールポイントの更新に失敗しました" }, status: :unprocessable_entity
+        raise ActiveRecord::Rollback unless current_user.profile.save
+
+        render json: { cheer_status: true }, status: :created
       end
+    rescue ActiveRecord::Rollback
+      render json: { error: "保有エールポイントの更新に失敗しました" }, status: :unprocessable_entity
     rescue ActiveRecord::RecordInvalid
       render json: { error: "ノートへのエールに失敗しました" }, status: :unprocessable_entity
     end

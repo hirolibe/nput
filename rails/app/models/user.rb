ebb9bd2d8c33559816f7d6506f64473fb4c5e780
hiro <hiro.libe.0930@gmail.com>
2024-11-01 15:25:27 +0900

    rubocopによるコード修正

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -9,10 +9,18 @@ class User < ApplicationRecord
   has_many :cheers, dependent: :destroy
   has_many :cheered_notes, through: :cheers, source: :note
 
-  has_many :following_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  has_many :following_relationships,
+           class_name: "Relationship",
+           foreign_key: :follower_id,
+           dependent: :destroy,
+           inverse_of: :follower
   has_many :followings, through: :following_relationships, source: :following
 
-  has_many :follower_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
+  has_many :follower_relationships,
+           class_name: "Relationship",
+           foreign_key: :following_id,
+           dependent: :destroy,
+           inverse_of: :following
   has_many :followers, through: :follower_relationships, source: :follower
 
   has_one :profile, dependent: :destroy

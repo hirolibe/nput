65b048d3c0363fe2903db822beb3159d8ed5e237
hiro <hiro.libe.0930@gmail.com>
2024-11-01 15:40:52 +0900

    マイグレーションファイルの修正

--- rails/db/migrate/20241031073814_add_follows_count_to_users.rb
+++ rails/db/migrate/20241031073814_add_follows_count_to_users.rb
@@ -1,4 +1,4 @@
-class AddFollowCountsToUsers < ActiveRecord::Migration[7.0]
+class AddFollowsCountToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :followings_count, :integer, default: 0, null: false
     add_column :users, :followers_count, :integer, default: 0, null: false

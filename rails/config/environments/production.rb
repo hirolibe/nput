2d771ad32a99175ba1a53ea42e7437541ca2b355
hiro <hiro.libe.0930@gmail.com>
2025-01-09 18:15:27 +0900

    デプロイ設定の変更

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -78,4 +78,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Default host for URL generation
+  Rails.application.routes.default_url_options[:host] = "https://n-put.com"
 end

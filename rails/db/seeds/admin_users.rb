b88c6437e5cc61b30fcfd839a511d2924e0a25aa
hiro <hiro.libe.0930@gmail.com>
2025-01-22 16:27:11 +0900

    rubocopによる指摘事項の修正

--- rails/db/seeds/admin_users.rb
+++ rails/db/seeds/admin_users.rb
@@ -1,5 +1,7 @@
 # 既存のユーザーのroleを"user"に設定
-User.where(role: nil).update_all(role: "user")
+User.where(role: nil).find_each do |user|
+  user.update(role: "user")
+end
 
 # 管理者ユーザーを作成
 User.find_or_create_by!(email: "hirolibe.nput@gmail.com") do |user|

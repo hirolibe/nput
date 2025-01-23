28548c18a455c0e21388b3b2cbfec64e9ddc6a3f
hiro <hiro.libe.0930@gmail.com>
2025-01-23 12:27:56 +0900

    ノート詳細ページのSSR化

--- next/src/contexts/AuthContext.ts
+++ next/src/contexts/AuthContext.ts
@@ -1,8 +1,9 @@
-import { createContext } from 'react'
+import { createContext, Dispatch, SetStateAction } from 'react'
 
 export interface AuthContextState {
   idToken: string | null | undefined
   isAuthLoading: boolean
+  setIsAuthLoading: Dispatch<SetStateAction<boolean>>
 }
 
 export const AuthContext = createContext<AuthContextState | undefined>(

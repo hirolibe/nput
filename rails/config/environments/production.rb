34b9a186f164e1d29a336e1db0cf5a4f1971d99e
hiro <hiro.libe.0930@gmail.com>
2025-01-24 02:44:37 +0900

    ホスト設定の修正

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -81,6 +81,5 @@ Rails.application.configure do
   # Default host for URL generation
   Rails.application.routes.default_url_options[:host] = "https://backend.n-put.com"
 
-  config.hosts.clear if ENV["RAILS_HOST"].present?
   config.hosts.concat(ENV["RAILS_HOST"].split(","))
 end

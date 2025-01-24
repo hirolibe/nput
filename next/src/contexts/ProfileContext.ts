02bb9920be92c10a7fc67ea3ddaaf1a003d66ac4
hiro <hiro.libe.0930@gmail.com>
2025-01-24 10:46:09 +0900

    ヘッダーの修正

--- next/src/contexts/ProfileContext.ts
+++ next/src/contexts/ProfileContext.ts
@@ -1,8 +1,6 @@
 import { createContext, Dispatch, SetStateAction } from 'react'
 
 export interface ProfileContextState {
-  currentUserName: string | undefined
-  currentUserNickname: string | undefined
   avatarUrl: string
   setAvatarUrl: Dispatch<SetStateAction<string>>
 }
--- next/src/providers/ProfileProvider.tsx
+++ next/src/providers/ProfileProvider.tsx
@@ -7,27 +7,16 @@ export interface ProfileProviderProps {
 }
 
 export const ProfileProvider = ({ children }: ProfileProviderProps) => {
-  const [currentUserName, setCurrentUserName] = useState<string | undefined>(
-    undefined,
-  )
-  const [currentUserNickname, setCurrentUserNickname] = useState<
-    string | undefined
-  >(undefined)
   const [avatarUrl, setAvatarUrl] = useState<string>('')
-
   const { profileData } = useProfile()
 
   useEffect(() => {
-    setCurrentUserName(profileData?.user.name)
-    setCurrentUserNickname(profileData?.nickname)
     setAvatarUrl(profileData?.avatarUrl ?? '')
-  }, [setCurrentUserName, setCurrentUserNickname, setAvatarUrl, profileData])
+  }, [setAvatarUrl, profileData])
 
   return (
     <ProfileContext.Provider
       value={{
-        currentUserName,
-        currentUserNickname,
         avatarUrl,
         setAvatarUrl,
       }}

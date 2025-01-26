2ecae8504d66c55c59efc131fa410aec902eeb4a
hiro <hiro.libe.0930@gmail.com>
2025-01-26 13:55:06 +0900

    rubocopによる指摘事項の修正

--- rails/app/serializers/profile_serializer.rb
+++ rails/app/serializers/profile_serializer.rb
@@ -12,16 +12,19 @@ class ProfileSerializer < ActiveModel::Serializer
 
   def x_link
     return unless object.x_username
+
     object.x_username
   end
 
   def github_link
     return unless object.github_username
+
     object.github_username
   end
 
   def avatar_url
     return unless object.avatar&.attached?
+
     Rails.application.routes.url_helpers.url_for(object.avatar)
   end
 end

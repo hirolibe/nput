fd2e6f38b2a3cec500e678878f2221efbf02578c
hiro <hiro.libe.0930@gmail.com>
2025-02-11 13:59:18 +0900

    データベース設定の修正

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -79,18 +79,13 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 
   config.after(:each) do

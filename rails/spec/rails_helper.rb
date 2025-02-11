58e3767d42619f341ffd2948b887015edc2c69ba
hiro <hiro.libe.0930@gmail.com>
2025-02-11 16:40:11 +0900

    データベースの設定の修正

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -78,16 +78,15 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.start
   end
 
   config.after(:each) do
     DatabaseCleaner.clean
-    FileUtils.rm_rf(Rails.root.join('tmp', 'storage'))
   end
 end

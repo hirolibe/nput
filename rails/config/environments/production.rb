c23e2d8128c26b130acea7ed18e3b203a78c36e9
hiro <hiro.libe.0930@gmail.com>
2025-03-10 20:19:52 +0900

    redisの設定修正

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   config.cache_store = :redis_cache_store, {
     url: ENV["REDIS_URL"],
-    expire_after: 1.day,
+    expires_in: 1.day,
   }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).

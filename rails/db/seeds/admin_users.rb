f6508e03191233e44e85365fec9d61a20c13f2fc
hiro <hiro.libe.0930@gmail.com>
2025-03-11 20:33:16 +0900

    Firebaseのuidで登録したユーザーをCognitoのuidに変更

--- rails/db/seeds/admin_users.rb
+++ rails/db/seeds/admin_users.rb
@@ -1,3 +1,3 @@
 # 管理者ユーザーを作成
-user = User.find_by!(email: "hirolibe.nput@gmail.com")
-user.update!(uid: "a7248ae8-60b1-7052-7330-e731b113b218")
+user = User.find_by!(email: "hiro.libe.prog@gmail.com")
+user.update!(uid: "57644af8-3041-70c4-43df-821a23ce62b3")

37d61474c4bbf1e9475dc45eff4e985599e2e281
hiro <hiro.libe.0930@gmail.com>
2025-03-11 16:00:50 +0900

    管理者のuid修正

--- rails/db/seeds/admin_users.rb
+++ rails/db/seeds/admin_users.rb
@@ -1,3 +1,3 @@
 # 管理者ユーザーを作成
 user = User.find_by!(email: "hirolibe.nput@gmail.com")
-user.update!(uid: "57c40ae8-80d1-70c1-ab46-4f703906835a")
+user.update!(uid: "a7248ae8-60b1-7052-7330-e731b113b218")
